f23b97fbdce064a609b144a3dcdafddee852276d
Daniel.Montivero <montivero.daniel.e@gmail.com>
2022-04-08 22:55:02 -0300

    Create fly-promo-preview-page

---
+++ fly-promo-preview-page/src/main/java/io/github/riojano0/flypromopreviewpage/StaticResourceServerVerticle.java
@@ -0,0 +1,32 @@
+package io.github.riojano0.flypromopreviewpage;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import io.vertx.ext.web.Router;
+import io.vertx.ext.web.handler.StaticHandler;
+
+public class StaticResourceServerVerticle extends AbstractVerticle {
+
+   @Override
+   public void start(Promise<Void> startPromise) throws Exception {
+      Router router = Router.router(vertx);
+      router.route("/*").handler(StaticHandler.create("static"));
+
+      vertx.createHttpServer()
+           .requestHandler(router)
+           .listen(config().getInteger("http:port", 3000),
+                 result -> {
+                    if(result.succeeded()) {
+                       startPromise.complete();
+                    } else {
+                       startPromise.fail(result.cause());
+                    }
+                 });
+   }
+
+   public static void main(String[] args) {
+      Vertx vertx = Vertx.vertx();
+      vertx.deployVerticle(new StaticResourceServerVerticle());
+   }
+}
